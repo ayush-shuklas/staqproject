627d855fbfaf615e9a9d9d5253e8ae750edb9a2b
Ayush Shukla <ayusshshuklaas@gmail.com>
2023-05-31 02:53:43 +0530

    Update Q2.rb

--- Q2.rb
+++ Q2.rb
@@ -1,14 +1,23 @@
+class Q2
+    def q2(contact_data,contacts)
+        length = contact_data.length
+        i=0
+        contacts.each_key do |value|
+            contacts[value][:email] = contact_data[i][0]
+            contacts[value][:address] = contact_data[i][1]
+            contacts[value][:phone] = contact_data[i][2]
+            i=i+1
+        end
+        puts contacts
+    end
+end
+
 contact_data = [
 ["joe@email.com", "123 Main st.", "555-123-4567"],
 ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]
 ]
+
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-length = contact_data.length
-i=0
-contacts.each_key do |value|
-    contacts[value][:email] = contact_data[i][0]
-    contacts[value][:address] = contact_data[i][1]
-    contacts[value][:phone] = contact_data[i][2]
-    i=i+1
-end
-puts contacts
+
+classobj = Q2.new
+classobj.q2(contact_data,contacts)
