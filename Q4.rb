f0e728ce4166350870c1f783139755b42403e2b7
Ayush Shukla <ayusshshuklaas@gmail.com>
2023-05-31 02:49:55 +0530

    Update Q4.rb

--- Q4.rb
+++ Q4.rb
@@ -1,7 +1,14 @@
+class Q4
+    def q4(array)
+        array.each do |value|
+            if(value == "green")
+                 puts "found green"
+             end
+         end
+     end
+ end
+    
 array = ["red","green","yellow","pink"]
+classobj = Q4.new
+classobj.q4(array)
 
-array.each do |value|
-    if(value == "green")
-        puts "found green"
-    end
-end
