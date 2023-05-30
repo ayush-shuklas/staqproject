2c4dd99f24a29c3d079b8bfbcdaede54bee1afcb
Ayush Shukla <ayusshshuklaas@gmail.com>
2023-05-31 02:42:12 +0530

    Update Q1.rb

--- Q1.rb
+++ Q1.rb
@@ -1,6 +1,51 @@
 # Q1.
 # part 1.
 
+
+class Q1
+    def q11(hash)
+        hash.each_key do |value|
+            if(value == "b")
+                 puts "Key b found with value"
+                 puts hash[value]
+             end
+         end
+         puts"Part one ends here"
+     end
+     
+     
+     def q12(hash)
+         hash["e"] = "5"
+         hash.each_key do |value|
+             print value," " ,hash[value]
+             puts " "
+         end
+         puts "part 2 ends here"
+     end
+     
+     
+     def q13(hash)
+         hash.each_key do |value|
+             if(hash[value].to_i < 3.5)
+                 hash.delete(value)
+             end
+         end
+         hash.each_key do |value|
+             print value," " ,hash[value]
+             puts " "
+         end
+         puts"3rd pard ends here"
+     end
+ end
+     
+     
+# part 3 starts here
+
+
+
+
+
+
 hash = {
   "a" => "1",
   "b" => "2",
@@ -8,38 +53,7 @@ hash = {
   "d" => "4"
 }
 
-hash.each_key do |value|
-    if(value == "b")
-        puts "Key b found with value"
-        puts hash[value]
-        puts "part 1 ends here"
-    end
-end
-
-# part 1 ends here
-
-
-# part 2 starts
-
-hash["e"] = "5"
-
- hash.each_key do |value|
-  print value," " ,hash[value]
-  puts " "
-end
-puts "part 2 ends here"
-# part 2 ends
-
-# part 3 starts here
-
-hash.each_key do |value|
-    if(hash[value].to_i < 3.5)
-        hash.delete(value)
-    end
-end
-
-hash.each_key do |value|
-  print value," " ,hash[value]
-  puts " "
-end
-puts "part 3 ends here"
+classobj = Q1.new
+classobj.q11(hash)
+classobj.q12(hash)
+classobj.q13(hash)
